f55fb959a59c6b563cc415d0802bd9f0219d5a92
AlienCoder <37622754+mrclcyn@users.noreply.github.com>
2022-01-13 18:04:55 +0100

    App.js maping the data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 
-function App() {
+function App({recipes}) {
+  console.log(recipes);
   return (
     <div className="App">
-         Home Component
+      <h1>Cook and Travel</h1>
+      <h3>Our recepies will surprise you!</h3>
+      {recipes.map((recipe, id) => {
+        return(
+          <div className="homeCountryBlocks" key={id}>
+            {recipe.fields.country}
+            <img className="homeCountryDishes" src={recipe.fields.imageSrc} alt={recipe.fields.country} />
+          </div>
+        )
+      })}
+
+
+
+
+
     </div>
   );
 }
