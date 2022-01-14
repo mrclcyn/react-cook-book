bbd48037a08fde2e7fa926bfe1c6bc6ff6bc53e4
kavitatandel <kavita2810@gmail.com>
2022-01-14 18:35:17 +0100

    updated dish

--- src/Dishes.js
+++ src/Dishes.js
@@ -1,26 +1,26 @@
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const Dishes = ({ recipes }) => {
-     
 
-  
-    console.log(recipes)
+
+
+    //console.log(recipes)
 
     return (
         <>
-            
+
             {recipes.map((items) => {
                 return (
-                        <>
-            
-                            <Link to={`/dishes/${items.fields.id}`}>
-                                <h1>
-                                    {items.fields.title} 
-                                </h1>
-                            </Link>
-                        
-                        </>
-                )   
+                    <>
+
+                        <Link to={`/dishe/${items.fields.id}`}>
+                            <h1>
+                                {items.fields.title}
+                            </h1>
+                        </Link>
+
+                    </>
+                )
             })}
         </>
     )
