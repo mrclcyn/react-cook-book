5510b4f3b36a702b814c165cdaff6c8aa7bb916e
Demodex420 <bastian.siegmund@icloud.com>
2022-01-16 15:54:09 +0100

    ..

--- src/Footer.js
+++ src/Footer.js
@@ -1,3 +1,14 @@
+<<<<<<< Updated upstream
+=======
+
+import { Facebook } from "react-bootstrap-icons";
+import { Instagram } from 'react-bootstrap-icons';
+import {Twitter} from 'react-bootstrap-icons'
+import { Youtube } from "react-bootstrap-icons";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+>>>>>>> Stashed changes
 const Footer = () => {
     return(
         <div>
